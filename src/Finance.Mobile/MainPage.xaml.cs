7307429b24a2fd099a0db2dbc9ed091186bd855c
Claudio Volnei <42410926+claudiovolnei@users.noreply.github.com>
2026-02-26 16:59:54 -0300

    Fix mobile startup splash loop by statically mounting Routes

--- src/Finance.Mobile/MainPage.xaml.cs
+++ src/Finance.Mobile/MainPage.xaml.cs
@@ -1,34 +1,9 @@
-using Microsoft.AspNetCore.Components.WebView.Maui;
-
 namespace Finance.Mobile;
 
 public partial class MainPage : ContentPage
 {
-    private bool _rootComponentAttached;
-    private const int SplashDelayMs = 2500;
-
     public MainPage()
     {
         InitializeComponent();
-        Loaded += OnPageLoaded;
-    }
-
-    private async void OnPageLoaded(object? sender, EventArgs e)
-    {
-        if (_rootComponentAttached)
-        {
-            return;
-        }
-
-        await Task.Delay(SplashDelayMs);
-
-        blazorWebView.RootComponents.Add(new RootComponent
-        {
-            Selector = "#app",
-            ComponentType = typeof(Components.Routes)
-        });
-
-        _rootComponentAttached = true;
-        Loaded -= OnPageLoaded;
     }
 }
