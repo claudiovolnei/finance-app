e3a1c729701dc864376f33452da1af632259d612
Claudio Volnei <42410926+claudiovolnei@users.noreply.github.com>
2026-02-26 16:20:14 -0300

    Adiciona atraso de splash na inicialização do app

--- src/Finance.Mobile/MainPage.xaml.cs
+++ src/Finance.Mobile/MainPage.xaml.cs
@@ -1,15 +1,34 @@
-﻿namespace Finance.Mobile;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace Finance.Mobile;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    private bool _rootComponentAttached;
+    private const int SplashDelayMs = 2500;
 
+    public MainPage()
+    {
+        InitializeComponent();
+        Loaded += OnPageLoaded;
+    }
 
+    private async void OnPageLoaded(object? sender, EventArgs e)
+    {
+        if (_rootComponentAttached)
+        {
+            return;
+        }
 
+        await Task.Delay(SplashDelayMs);
 
+        blazorWebView.RootComponents.Add(new RootComponent
+        {
+            Selector = "#app",
+            ComponentType = typeof(Components.Routes)
+        });
 
-
+        _rootComponentAttached = true;
+        Loaded -= OnPageLoaded;
+    }
 }
